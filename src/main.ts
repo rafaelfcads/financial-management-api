7544a756fa182ec15aa5839629ed4cb7bf971c8d
Rafael Reis <rafaelfc.ads@gmail.com>
2018-03-15 00:25:13 -0300

    application configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
-	await app.listen(3000);
+	await app.listen(config.get('app.port'));
 }
 bootstrap();
